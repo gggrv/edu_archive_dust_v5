bfc48b9509060065a22fb3753a07ae5ae3a68d70
gggrv <43758914+gggrv@users.noreply.github.com>
2023-10-07 23:00:01 +0300

    [ok] update signal is now optional

--- sparkling/grimoire/pyqt5/PlaylistSelector.py
+++ sparkling/grimoire/pyqt5/PlaylistSelector.py
@@ -161,7 +161,7 @@ class PlaylistSelector( NodeViewer ):
         
         super( PlaylistSelector, self ).launch_selection_editor()
         
-    def _accept_programmatic_edits( self, df ):
+    def _accept_programmatic_edits( self, df, tell_everyone=True ):
         
         # For careful manual use only.
         
@@ -183,7 +183,8 @@ class PlaylistSelector( NodeViewer ):
         self.reapply_columns_to_hide( columns_to_hide=PLAYLIST_COLUMNS_TO_SHOW, appropriate_reverse=True )
         
         # tell everyone to update
-        self.PLAYLIST_EDITED.emit( df )
+        if tell_everyone:
+            self.PLAYLIST_EDITED.emit( df )
         
     def _accept_selection_edits_event( self, changes, db_name ):
     
