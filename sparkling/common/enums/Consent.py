bcf6ef250b0486a0938fa70bfc8b4fcbe07d966c
gggrv <43758914+gggrv@users.noreply.github.com>
2023-10-07 03:00:03 +0300

    [ok] consent string enum

---
+++ sparkling/common/enums/Consent.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+#---------------------------------------------------------------------------+++
+#
+
+# logging
+import logging
+log = logging.getLogger(__name__)
+
+class EConsent:
+    
+    # I want to operate with simple strings.
+    
+    CONSENT = '+'
+    DECLINE = '-'
+                
+#---------------------------------------------------------------------------+++
+# end 2023.10.07
+# created
