9439fa9e271b2ff68df0597c501323a662b547b8
gggrv <43758914+gggrv@users.noreply.github.com>
2023-10-15 01:46:10 +0300

    [ok] simplified context common functions

--- sparkling/contech/main.py
+++ sparkling/contech/main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 #---------------------------------------------------------------------------+++
-# Tools for ConTeXt projects.
+# Common context-unaware tools for any ConTeXt projects.
 
 # logging
 import logging
@@ -16,87 +16,80 @@ import pandas as pd
 from sparkling.contech.Conventions import Conventions
 from sparkling.contech.Commands import Commands
 from sparkling.common import (
-    readf, savef, chop, select_files )
+    readf, chop,
+    select_files,
+    replace_text_in_file,
+    ColumnsFoundTexts, find_text_in_file )
 
-def get_components( product_root, prefix=Conventions.COMPONENT_NAME_PREFIX, dot_ext='.tex' ):
+class ColumnsFoundTags( ColumnsFoundTexts ):
+    
+    tag_name = 'tag'
+    tag_value = 'value'
+    
+    product_root = 'product_root'
+    project_root = 'project_root'
+    
+    texts = {
+        tag_name: 'Tag name',
+        tag_value: 'Tag value',
+        product_root: 'Product root',
+        project_root: 'Project root'
+        }.update( ColumnsFoundTexts.texts )
 
-    # Gets all c_whatever.tex in product_root.
-    # Returns { whatever: full_path_to_c_whatever.tex }.
-
-    if not os.path.isdir( product_root ):
-        raise FileNotFoundError
-
-    components = select_files( product_root, prefix, dot_ext, True )
-
-    table = {}
-
-    for src in components:
-
-        _,f = os.path.split(src)
-        name,_ = os.path.splitext(f)
-        
-        if name.startswith( Conventions.COMPONENT_NAME_PREFIX ):    
-            name = name.replace( Conventions.COMPONENT_NAME_PREFIX, '', 1 )
-
-        table.setdefault( name, src )
-
-    return table
-
-def get_products( project_root, prefix=Conventions.PRODUCT_NAME_PREFIX, dot_ext='.tex' ):
-
-    # Gets all prd_whatever.tex in project_root.
-    # Returns { whatever: full_path_to_prd_whatever.tex }.
-
+def get_existing_products( project_root, prefix=Conventions.PRODUCT_NAME_PREFIX, dot_ext='.tex' ):
+    # Gets all existing product.tex in given project.
     if not os.path.isdir( project_root ):
-        raise FileNotFoundError
+        return []
+    return select_files( project_root, prefix, dot_ext, True )
 
-    products = select_files( project_root, prefix, dot_ext, True )
+def get_existing_components( root_folder, this_is_project, prefix=Conventions.COMPONENT_NAME_PREFIX, dot_ext='.tex' ):
+    
+    # Gets all existing component.tex in a given product / project.
+    # `this_is_project`=True means `look for all the components you can find`,
+    # `this_is_project`=False means `look only for components in this folder`.
+    
+    if not os.path.isdir( root_folder ):
+        return []
+    
+    return select_files( root_folder, prefix, dot_ext, this_is_project )
 
-    table = {}
+def get_referenced_componenets( product_src, return_paths=True ):
 
-    for src in products:
-
-        _,f = os.path.split(src)
-        name,_ = os.path.splitext(f)
-
-        if name.startswith( Conventions.PRODUCT_NAME_PREFIX ):    
-            name = name.replace( Conventions.PRODUCT_NAME_PREFIX, '', 1 )
-            
-        table.setdefault( name,src )
-
-    return table
-
-def get_referenced_componenets( product_src ):
-
-    # Gets all c_whatever.tex referenced in prd_whatever.tex.
-    # Returns { whatever: full_path_to_c_whatever.tex }.
+    # Gets all components.tex referenced in product.tex.
+    # They may not actually exist on disk.
+    # Prefixes don't matter.
 
     if not os.path.isfile(product_src):
-        raise FileNotFoundError
+        # no file, nothing to look for
+        return []
 
+    # obtain actual root folder
     prd_root = os.path.dirname( product_src )
-    table = {}
-
+    
+    # parse product
+    result = []
+    c = Commands
     for line in readf( product_src, join_lines=False ):
         
-        if not f'\\component {Conventions.COMPONENT_NAME_PREFIX}' in line:
+        if not f'{c.component} ' in line:
             continue
 
-        name = chop( line, '\component ', None )
-        if ' ' in name: name=chop(name,None,' ')
-        if '%' in name: name=chop(name,None,'%')
-        if '\n' in name: name=chop(name,None,'\n')
-
-        if name.startswith( Conventions.COMPONENT_NAME_PREFIX ):    
-            name = name.replace( Conventions.COMPONENT_NAME_PREFIX, '', 1 )
+        basename = chop( line, f'{c.component} ', None )
+        if ' ' in basename: basename=chop(basename,None,' ')
+        if '%' in basename: basename=chop(basename,None,'%')
+        if '\n' in basename: basename=chop(basename,None,'\n')
             
-        src = os.path.join( prd_root, f'{name}.tex' )
-        
-        table.setdefault( name , src )
+        if return_paths:
+            result.append( os.path.join( prd_root, f'{basename}.tex' ) )
+        else:
+            result.append( basename )
 
-    return table
+    return result
 
-def get_unreferenced_components( product_src, prefix=Conventions.COMPONENT_NAME_PREFIX ):
+def get_unreferenced_components(
+        product_src,
+        prefix=Conventions.COMPONENT_NAME_PREFIX, dot_ext='.tex',
+        return_referenced=False ):
 
     # Finds all unreferenced components in a product.
 
@@ -105,36 +98,40 @@ def get_unreferenced_components( product_src, prefix=Conventions.COMPONENT_NAME_
 
     prd_root = os.path.dirname( product_src )
 
-    existing = get_components( prd_root, prefix=prefix )
-    referenced = get_referenced_componenets(product_src)
+    existing = get_existing_components( prd_root, False, prefix=prefix, dot_ext=dot_ext )
+    referenced = get_referenced_componenets( product_src, return_paths=True )
 
-    table = {}
+    result = []
+    for f in existing:
+        if not f in referenced:    
+            result.append( f )
 
-    for name in existing:
-        if name in referenced:
-            # it exists and it was referenced, everything ok
-            continue
-        table.setdefault( name, existing[name] )
+    if return_referenced:
+        return result, referenced
+    return result
 
-    return table
-
-def get_component_tags( src, tags=None ):
+def parse_component_tags( src, tags=None ):
 
     # Parses source file and returns dictionary with
-    # found unincapsulated tags and their values.
+    # found unencapsulated tags and their values.
 
-    # For now only commands are supported.
+    # Word `tag` describes 2 things:
+    # - a `context` `command`
+    # - a `context` `switch`
+    # For now only `commands` are supported.
 
     if tags is None:
         # TODO
         # allow to get all tags that exist in file
-        tags=[]
+        log.error( 'please provide specific tags, not doing anything' )
+        return []
 
     TEXT = readf(src)
     TEXTLEN = len(TEXT)
 
     found = []
 
+    c = ColumnsFoundTags
     for tag in tags:
 
         # how many times does this tag appear in text?
@@ -144,13 +141,13 @@ def get_component_tags( src, tags=None ):
             iloc = TEXT.find( tag, iloc )
 
             L = iloc+len(tag) # next char iloc
-            # text ended, tag value doesnt exist
+            # text ended, tag value doesn't exist
             if TEXTLEN<=L:
                 PLACEHOLDER = {
-                    'src': src,
-                    'iloc': iloc,
-                    'tag': tag,
-                    'value': pd.NA,
+                    c.file_src: src,
+                    c.iloc: iloc,
+                    c.tag_name: tag,
+                    c.tag_value: pd.NA,
                     }
                 found.append( PLACEHOLDER )
 
@@ -161,10 +158,10 @@ def get_component_tags( src, tags=None ):
                 R = TEXT.find(']',L)
                 value = TEXT[ L+1 : R ]
                 PLACEHOLDER = {
-                    'src': src,
-                    'iloc': iloc,
-                    'tag': tag,
-                    'value': value,
+                    c.file_src: src,
+                    c.iloc: iloc,
+                    c.tag_name: tag,
+                    c.tag_value: value,
                     }
                 found.append( PLACEHOLDER )
 
@@ -173,108 +170,96 @@ def get_component_tags( src, tags=None ):
                 R = TEXT.find('}',L)
                 value = TEXT[ L+1 : R ]
                 PLACEHOLDER = {
-                    'src': src,
-                    'iloc': iloc,
-                    'tag': tag,
-                    'value': value,
+                    c.file_src: src,
+                    c.iloc: iloc,
+                    c.tag_name: tag,
+                    c.tag_value: value,
                     }
                 found.append( PLACEHOLDER )
 
-    return pd.DataFrame( found, columns=['src','iloc','tag','value'] )
+    return pd.DataFrame( found, columns=list(c.texts) )
 
-def get_product_tags( product_root, tags, skip_components, prefix=Conventions.COMPONENT_NAME_PREFIX ):
-
-    components = get_components( product_root, prefix=prefix )
+def parse_product_tags(
+        product_root, tags=None,
+        skip_components=None,
+        prefix=Conventions.COMPONENT_NAME_PREFIX, dot_ext='.tex' ):
     
+    # It is possible that prd.tex contains tags as well.
+    # I don't parse them - only underlying components.
+
+    c = ColumnsFoundTags
+
+    if tags is None:
+        # TODO
+        # allow to get all tags that exist in file
+        log.error( 'please provide specific tags, not doing anything' )
+        return pd.DataFrame( [], columns=list(c.texts) )
+    
+    # make sure i actually have components to parse
+    components = get_existing_components( product_root, False, prefix=prefix, dot_ext=dot_ext )
     if len( components )==0:
-        return pd.DataFrame( [], columns=['src','iloc','tag','value'] )
+        return pd.DataFrame( [], columns=list(c.texts) )
     
     dfs = []
 
-    for name, src in components.items():
-        if name in skip_components or src in skip_components:
+    for src in components:
+        basename = os.path.basename( src )
+        name = basename.replace( prefix, '' ) if prefix is not None else basename
+        if name in skip_components or basename in skip_components or src in skip_components:
             continue
-        found = get_component_tags( src, tags )
+        found = parse_component_tags( src, tags=tags )
         dfs.append( found )
         
     df = pd.concat( dfs, axis=0 )
-    df.sort_values( ['iloc','src'], inplace=True )
+    df.sort_values( [c.iloc,c.file_src], inplace=True )
     df.reset_index( drop=True, inplace=True )
     return df
 
-def replace_in_file( src, replacements ):
+def replace_in_product( product_root, replacements,
+    prefix=Conventions.COMPONENT_NAME_PREFIX, dot_ext='.tex' ):
 
-    text = readf(src)
-    changed_text = False
+    components = get_existing_components( product_root, False, prefix=prefix, dot_ext=dot_ext )
+    for src in components:
+        replace_text_in_file( src, replacements, inplace=True )
 
-    for pair in replacements:
+def replace_in_project( project_root, replacements,
+    product_prefix=Conventions.PRODUCT_NAME_PREFIX,
+    component_prefix=Conventions.COMPONENT_NAME_PREFIX,
+    product_dot_ext='.tex', component_dot_ext='.tex' ):
 
-        if pair[0] in text:
-            text = text.replace( pair[0],pair[1] )
-            changed_text = True
-
-    if changed_text:
-        savef( src, text )
-
-def replace_in_product( product_root, replacements, prefix=Conventions.COMPONENT_NAME_PREFIX ):
-
-    components = get_components( product_root, prefix=prefix )
-    for _, src in components.items():
-        replace_in_file( src, replacements )
-
-def replace_in_project( project_root, replacements ):
-
-    prds = get_products( project_root )
-    for src in prds.values():
+    prds = get_existing_products( project_root, prefix=product_prefix, dot_ext=product_dot_ext )
+    for src in prds:
         prd_root = os.path.dirname( src )
-        replace_in_product( prd_root, replacements )
+        replace_in_product( prd_root, replacements, prefix=component_prefix, dot_ext=component_dot_ext )
 
-def find_in_file( src, texts ):
+def find_in_product( product_root, texts, prefix=Conventions.COMPONENT_NAME_PREFIX, dot_ext='.tex' ):
 
-    # Parses source file and returns dictionary with
-    # found things.
-
-    TEXT = readf(src)
-
-    found = []
-
-    for text in texts:
-
-        # how many times does this tag appear in text?
-        # iterate each found
-        iloc = 0
-        for _ in range( TEXT.count( text ) ):
-            iloc = TEXT.find( text, iloc )
-            PLACEHOLDER = {
-                'src': src,
-                'iloc': iloc,
-                'text': text,
-                }
-            found.append( PLACEHOLDER )
-
-    return pd.DataFrame(found)
-
-def find_in_product( product_root, texts, prefix=Conventions.COMPONENT_NAME_PREFIX ):
-
+    cs = get_existing_components( product_root, False, prefix=prefix, dot_ext=dot_ext )
+    if len(cs) == 0:
+        return pd.DataFrame( [], columns=list(ColumnsFoundTags.texts) )
+    
     dfs = []
-
-    cs = get_components( product_root, prefix=prefix )
-    for _, c_src in cs.items():
-        df = find_in_file( c_src, texts )
-        df['prd'] = product_root
+    for component in cs:
+        df = pd.DataFrame( find_text_in_file(component,texts), columns=list(ColumnsFoundTags.texts) )
+        df[ColumnsFoundTags.product_root] = product_root
         dfs.append( df )
 
     return pd.concat( dfs, axis=0, sort=False )
 
-def find_in_project( project_root, texts ):
+def find_in_project( project_root, texts,
+    product_prefix=Conventions.PRODUCT_NAME_PREFIX,
+    component_prefix=Conventions.COMPONENT_NAME_PREFIX,
+    product_dot_ext='.tex', component_dot_ext='.tex' ):
 
+    prds = get_existing_products( project_root, prefix=product_prefix, dot_ext=product_dot_ext )
+    if len(prds) == 0:
+        return pd.DataFrame( [], columns=list(ColumnsFoundTags.texts) )
+    
     dfs = []
-
-    prds = get_products( project_root )
     for prd_src in prds.values():
         prd_root = os.path.split( prd_src )[0]
         df = find_in_product( prd_root, texts )
-        df['project'] = project_root
+        df[ColumnsFoundTags.project_root] = project_root
         dfs.append( df )
 
     return pd.concat( dfs, axis=0, sort=False )
@@ -283,6 +268,9 @@ def render_product( prd_src, context_console_command='context' ):
     
     # Renders chosen .tex file as if it was
     # a product.
+    
+    # I assume that `context` `folder with binaries` is added
+    # to system `PATH`.
     
     prev_root = os.getcwd()
     prd_root, basename = os.path.split( prd_src )
@@ -296,8 +284,10 @@ def render_product( prd_src, context_console_command='context' ):
     return os.path.join( prd_root, f'{prd_name}.pdf' )
 
 def get_references_definitions_for_product(
-        product_root, tags=[Commands.REFERENCE2], skip_components=[],
-        prefix=Conventions.COMPONENT_NAME_PREFIX
+        product_root,
+        tags=[Commands.reference],
+        skip_components=None,
+        prefix=Conventions.COMPONENT_NAME_PREFIX, dot_ext='.tex'
         ):
 
     # Parses given product (all existing components).
@@ -309,17 +299,17 @@ def get_references_definitions_for_product(
     # text for each known reference value
 
     # get tag values - who was referenced?
-    df = get_product_tags( product_root, tags, skip_components, prefix=prefix )
-    found_references = df['value'].unique()
+    df = parse_product_tags( product_root, tags=tags, skip_components=skip_components, prefix=prefix, dot_ext=dot_ext )
+    found_references = df[ColumnsFoundTags.tag_value].unique()
 
     # generate valid .tex contents
     lines = []
     for reference in found_references:
-        line = Commands.REFERENCE.replace('%value%',reference).replace('%text%',reference)
+        line = Commands.reference_with_value.replace('%value%',reference).replace('%text%',reference)
         lines.append(line)
         
     return lines
     
 #---------------------------------------------------------------------------+++
-# end 2023.05.22
-# propagated prefix
+# end 2023.10.14
+# simplified
