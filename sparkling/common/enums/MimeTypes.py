789611bbb4f263bf429f88ce34faa8c2e43915eb
gggrv <43758914+gggrv@users.noreply.github.com>
2023-10-20 22:58:48 +0300

    [ok] common enum with mime types

---
+++ sparkling/common/enums/MimeTypes.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+#---------------------------------------------------------------------------+++
+
+from sparkling.common.BaseColumns import BaseColumns
+
+class EMimeTypes( BaseColumns ):
+    
+    # standard
+    
+    TEXT_PLAIN = 'text/plain'
+    
+    # custom
+            
+#---------------------------------------------------------------------------+++
+# 2023.10.20
+# simplified
