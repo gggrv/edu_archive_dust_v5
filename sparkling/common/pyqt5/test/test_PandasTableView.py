9acc4973f05fd1f85b7e35f0c58356d89033c1d2
gggrv <43758914+gggrv@users.noreply.github.com>
2024-05-01 21:47:12 +0300

    [save] added license per-file

--- sparkling/common/pyqt5/test/test_PandasTableView.py
+++ sparkling/common/pyqt5/test/test_PandasTableView.py
@@ -1,4 +1,18 @@
 # -*- coding: utf-8 -*-
+#Python utility "Test for Pandas TableView". Allows to manually inspect and documents some of the functionality of a "Pandas TableView" PyQt5 object. Copyright (C) 2023 Anna Anikina
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #---------------------------------------------------------------------------+++
 # Tests whether some PyQt5 widget works
